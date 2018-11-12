e86cce58ee5546de98950988bee029f03217f28c
Pergatory23 <gharbi.rafik@hotmail.fr>
2018-11-12 13:26:55 +0100

    Initial commit

---
+++ Assets/DartBoardBehavior.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DartBoardBehavior : MonoBehaviour {
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.tag.Equals("Dart")) 
+		{
+			col.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+		}
+	}
+}
